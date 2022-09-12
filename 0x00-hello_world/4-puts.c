504c6784f3be724c332714d343112bc675aa12f0
antoh <otienoantoh@gmail.com>
2022-09-12 15:16:04 -0700

    4 puts c correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 /*
- main - Entry point
- Return: Always 0 (Success)
+* main - Entry point
+* Return: Always 0 (Success)
 */
 int main(void)
 {
+/*
+ * I used puts	
+ */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
