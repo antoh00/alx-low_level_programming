29411a73cc2578b13bdbd2bd228b5fa7e0b573b2
Antony <otienoantoh@gmail.com>
2022-10-04 13:35:22 +0300

    adjust betty style

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main -  print name
+ *main -  print name
  *@argc: int.
  *@argv: char.
  * Return: Always 0.
- */
+*/
 int main(int argc, __attribute__((unused)) char *argv[])
 {
 	printf("%d\n", argc - 1);
